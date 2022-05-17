2bac5a3f903197227ae91a4014655b73cff70b01
Adeniyi Funmbi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2022-05-17 22:12:58 +0100

    ch: use yarn workspaces

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {createRoot} from 'react-dom/client';
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 import App from './App';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
+	uri: 'http://localhost:4000/graphql',
+	cache: new InMemoryCache(),
 });
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
 root.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+	<ApolloProvider client={client}>
+		<App/>
+	</ApolloProvider>,
 );
--- server/src/services/orders.ts
+++ server/src/services/orders.ts
@@ -1,26 +1,26 @@
-import { firestore } from 'firebase-admin';
+import {firestore} from 'firebase-admin';
+import {OrderResponse} from '../graphql/code_generated';
 import Firestore = firestore.Firestore;
-import { OrderResponse } from '../graphql/code_generated';
 
 const OrdersService = (db: Firestore) => {
-  const database = db;
-
-  const getOrder = async (id: string):Promise<OrderResponse> => {
-    const orderDocRef = await database.collection('orders').doc(id);
-    // await orderDocRef.set(inputOrder[0], {merge: true})
-    const orderDoc = await orderDocRef.get();
-
-    if (!orderDoc.exists) {
-      throw new Error(`Order ${id} does not exist`);
-    } else {
-      const respData = { ...orderDoc.data(), uid: id };
-      return respData as OrderResponse;
-    }
-  };
-
-  return {
-    getOrder,
-  };
+	const database = db;
+	
+	const getOrder = async (id: string): Promise<OrderResponse> => {
+		const orderDocRef = database.collection('orders').doc(id);
+		// await orderDocRef.set(inputOrder[0], {merge: true})
+		const orderDoc = await orderDocRef.get();
+		
+		if (!orderDoc.exists) {
+			throw new Error(`Order ${id} does not exist`);
+		} else {
+			const respData = {...orderDoc.data(), uid: id};
+			return respData as OrderResponse;
+		}
+	};
+	
+	return {
+		getOrder,
+	};
 };
 
 export default OrdersService;
