bf4b6be2a9d50c43f2810cec9a0d04e5613f6582
Adeniyi Funmbi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2022-05-17 23:01:30 +0100

    regenerate yarn workspaces lock file

--- server/src/firebase.ts
+++ server/src/firebase.ts
@@ -1,9 +1,9 @@
-import admin, { ServiceAccount } from 'firebase-admin';
-import serviceAccount from '../serviceAccountKey.json';
+import admin, {ServiceAccount} from 'firebase-admin';
+import serviceAccount from '../../serviceAccountKey.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
-  databaseURL: 'https://construyo-coding-challenge.firebaseio.com',
+	credential: admin.credential.cert(serviceAccount as ServiceAccount),
+	databaseURL: 'https://construyo-coding-challenge.firebaseio.com',
 });
 
 export const db = admin.firestore();
