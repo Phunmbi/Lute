dca127204790a427e5ae7131dc2ec07f2cc0ac67
Adeniyi Funmbi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2022-05-18 00:00:22 +0100

    suppress firebase ts issue

--- server/src/firebase.ts
+++ server/src/firebase.ts
@@ -1,8 +1,9 @@
-import {credential, firestore, initializeApp, ServiceAccount} from 'firebase-admin';
+import admin, {credential, firestore, ServiceAccount} from 'firebase-admin';
 import serviceAccount from '../../serviceAccountKey.json';
 import Firestore = firestore.Firestore;
 
-initializeApp({
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+admin.initializeApp({
 	credential: credential.cert(serviceAccount as ServiceAccount),
 	databaseURL: 'https://construyo-coding-challenge.firebaseio.com',
 });
