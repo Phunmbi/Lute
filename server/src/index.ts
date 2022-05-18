0cc9a2e9b9d45fc3fc0f9351c0a8df8b5b1b47d9
Adeniyi Funmbi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2022-05-18 02:05:02 +0100

    move apollo client uri to env variable and add health endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,18 +3,24 @@ import {createServer} from 'http';
 import createApolloServer from './apollo-server';
 import {db} from './firebase';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
 const app: Express = express();
+const port = 4000 || process.env.PORT;
 
 async function start() {
 	const httpServer = createServer(app);
 	const apolloServer = await createApolloServer(db, httpServer, app);
 	
-	app.listen('4000', () => {
-		console.log('GraphQL API Server listening on 4000');
-		console.log(`sandbox: http://localhost:4000${apolloServer.graphqlPath}`);
+	app.listen(port, () => {
+		console.log(`GraphQL API Server listening on ${port}`);
+		console.log(`sandbox: http://localhost:${port}${apolloServer.graphqlPath}`);
 	});
 }
 
 start().catch((e) => {
 	console.error(e);
+});
+
+app.get("/health", (req, res) => {
+	return res.status(200).send({message: "lute graphql server is healthy"});
 });
